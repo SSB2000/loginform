64e255ae5d3c51c025edce01fdfe32b7ac02f9e4
shubham singh bohra <shubhambohra292@gmail.com>
2021-07-31 10:48:10 +0530

    complete project

--- InternProject/site.js
+++ InternProject/site.js
@@ -1,11 +1,32 @@
-$('#form-submit').click (function() {
+var form1 = document.getElementById("personal");
+var form2 = document.getElementById("address");
+var form3 = document.getElementById("edu");
 
-	event.preventDefault();
+var b1n = document.getElementById("button1n");
+var b2p = document.getElementById("button2p");
+var b2n = document.getElementById("button2n");
+var b3p = document.getElementById("button3p");
+var b3n = document.getElementById("button3n");
 
-	if ($("#username").val() == "admin" && $("#password").val() == "password") {
-		$('#valid').css('display', 'block').css('width', '250px');
-	} else {
-		$('#error').css('display', 'block');
-	}
+b1n.addEventListener("click", () => {
+    form1.style.left = "-900px";
+    form2.style.left = "20px";
 });
 
+b2p.addEventListener("click", () => {
+    form1.style.left = "20px";
+    form2.style.left = "900px";
+});
+
+b2n.addEventListener("click", () => {
+    form2.style.left = "-900px";
+    form3.style.left = "20px";
+});
+b3p.addEventListener("click", () => {
+    form2.style.left = "20px";
+    form3.style.left = "900px";
+});
+b1n.addEventListener("click", () => {
+    form1.style.left = "-900px";
+    form2.style.left = "20px";
+});
